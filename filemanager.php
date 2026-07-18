078d39af519186ba8274238396235078a83173a8
demo-vscode <aziztidur@gmail.com>
2026-07-18 08:52:39 +0700

    Update filemanager.php

--- filemanager.php
+++ filemanager.php
@@ -9,7 +9,7 @@ $use_auth = true;
 
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 $auth_users = array(
-    'milmnava' => '',
+    'fm_admin' => 'milmnava',
 );
 
 // Enable highlight.js (https://highlightjs.org/) on view's page
